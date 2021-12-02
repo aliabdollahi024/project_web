39cc8b76723e3f68fc3741dedab88919cea76205
ali abdollahi <aliabdollahi024a@gmail.com>
2021-12-02 21:11:46 +0330

    javascript code change

--- scripts.js
+++ scripts.js
@@ -1,16 +1,4 @@
 // JavaScript
-/*function submit()
-{
-    String1 = "https://api.genderize.io/?name=";
-    console.log("ali" + document.getElementById("firstNameinput").value)
-    String1 = String1 + document.getElementById("firstNameinput").value;
-    console.log(String1)
-    fetch(String1).then((resp)=>resp.json()).then((data)=>(document.getElementById("gender").innerText = data , 
-    document.getElementById("probability").innerText = data["probability"] , 
-    console.log(data)
-    ));
-}
-document.getElementById("submitButton").onclick = submit();*/
 
 var current_name = "";
 var curent_gender = "";
@@ -29,40 +17,67 @@ function submit()
     document.getElementById("probability").innerText = data["probability"] , curent_gender =  data["gender"],
     console.log(data)
     ));
+
+    if (localStorage.getItem(current_name)!=null)
+    {
+        document.getElementById("history").innerHTML = current_name+" = "+localStorage.getItem(current_name);
+    }
+    else
+    {
+        document.getElementById("history").innerHTML ="";
+        
+    }
     
 }
 
-function save()
+function saveRequest()
 {
-    if (document.getElementById("html1").checked == true)
+
+    if (document.getElementById("firstNameinput").value ==current_name)
+    {if(localStorage.getItem(current_name)==null){
+        localStorage.setItem(current_name,curent_gender);
+    }
+        
+    localStorage.setItem(current_name,curent_gender);
+    document.getElementById("history").innerHTML = current_name+" = "+localStorage.getItem(current_name);
+}
+else{alert("first submit then try again");}
+    
+}
+
+function saveMyanswer()
+{
+
+
+    if (document.getElementById("firstNameinput").value ==current_name)
+    {if (document.getElementById("html1").checked == true)
     {
         localStorage.setItem(current_name,"Male");
         document.getElementById("html1").checked = false;
+        document.getElementById("history").innerHTML = current_name+" = "+localStorage.getItem(current_name);
 
     }
     else if(document.getElementById("html2").checked == true)
     {
         localStorage.setItem(current_name,"Female");
         document.getElementById("html2").checked = false;
+        document.getElementById("history").innerHTML = current_name+" = "+localStorage.getItem(current_name);
     }
-    else
-    {
-        
-        console.log(current_name)
-        if(localStorage.getItem(current_name)==null){
-            console.log("wwwwwwwwww")
-            localStorage.setItem(current_name,curent_gender);
-        }
-        
-        localStorage.setItem(current_name,curent_gender);
-    }  
+}
+else
+{alert("first submit then try again")}
+
+    
 }
 
 function clear()
 {
 localStorage.removeItem(current_name);
+document.getElementById("history").innerHTML ="";
 }
+
 document.getElementById("clearButton").addEventListener("click", clear);
 document.getElementById("SubmitButton").addEventListener("click", submit);
-document.getElementById("SaveButton").addEventListener("click", save);
+document.getElementById("SaveRequestButton").addEventListener("click", saveRequest);
+document.getElementById("SaveMyPredictionButton").addEventListener("click", saveMyanswer);
 
